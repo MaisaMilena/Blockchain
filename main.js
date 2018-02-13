64a12e21c0a8057314c8de06442943a3962142f5
Maisa <maisa.milena@gmail.com>
2018-02-13 01:55:21 -0200

    working on cryptografy

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 // import 
 const SHA256 = require('crypto-js/sha256');
+const AES = require('crypto-js/aes');
 
 // structure 
 class Block {
 	constructor(index, timestamp, data, previousHash = ''){
 		this.index = index;
 		this.timestamp = timestamp;
-		this.data = data;
+		this.data = this.encryptData();
 		this.previousHash = previousHash;
 		this.hash = this.calculateHash();
 	}
@@ -15,6 +16,15 @@ class Block {
 	*/
 	calculateHash(){
 		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+	}
+
+	encryptData(){
+		return AES.AES.encrypt(this.data, 'key');
+	}
+
+	decryptData(key){
+		var bytes  = AES.AES.decrypt(this.data, key);
+		var plaintext = bytes.toString(AES.enc.Utf8);
 	}
 }
 
@@ -37,6 +47,29 @@ class Blockchain {
 		newBlock.hash = newBlock.calculateHash();
 		this.chain.push(newBlock);
 	}
+
+	// verify the current and previous block's hash 
+	isChainValid(){
+		for (let i = 1; i < this.chain.length; i++) {
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i - 1];
+			
+			console.log('Hash: '+currentBlock.hash+'previousHash: '+previousBlock.hash);
+			// verify if the hash of the current block is valid
+			if (currentBlock.hash !== currentBlock.calculateHash()) {
+				return false;
+			}
+
+			// check if the current block point to the correct previous block 
+			if (currentBlock.previousHash !== previousBlock.hash) {
+				return false;
+			}
+		}
+
+		//the chain is valid
+		return true;
+	}
+
 }
 
 // main 
@@ -45,12 +78,14 @@ maisaChain.addBlock(new Block(1, "19/12/2017", "testing"));
 maisaChain.addBlock(new Block(2, "19/12/2017", "testing second block"));
 maisaChain.addBlock(new Block(3, "19/12/2017", "testing third block"));
 
+console.log('Is blockchain valid? ' + maisaChain.isChainValid());
 
-console.log(JSON.stringify(maisaChain, null, 4));
+maisaChain.chain[1].data =  "changing the kiii";
+maisaChain.chain[1].hash = maisaChain.chain[1].calculateHash();
 
+//console.log(JSON.stringify(maisaChain, null, 5));
 
-
-
+console.log('Is blockchain valid? ' + maisaChain.isChainValid());
 
 
 
